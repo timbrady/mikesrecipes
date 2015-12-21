e6aa0d805ad74cda08b6caeb5414863c3fa0eafc
timbrady <timbrady@Tims-MacBook-Air.local>
2015-12-21 11:22:37 -0800

    js search update

--- search.php
+++ search.php
@@ -1,6 +1,3 @@
-<!DOCTYPE html>
-<html><head>
-
 <?php
   session_start();  
   if (@$_SESSION['auth'] != "yes")                   
@@ -10,40 +7,72 @@
   }
 ?>
 
+<!DOCTYPE html>
+<html><head>
 <title>Mike's Recipes</title>
 <meta http-equiv="Content-Type" content="text/html;charset=UTF-8">
-</head>
-
-<body>
+  <link rel="stylesheet" href="../jscript/jquery-ui-1.11.4/themes/smoothness/jquery-ui.css">
+  <script src="../jscript/jquery-1.11.1.js"></script>
+  <script src="../jscript/jquery-ui-1.11.4/jquery-ui.js"></script>
+  
+  <style>
 	
-<script>
-function showHint(str)
-{
-    if (str.length==0)
-    {
-        document.getElementById("txtHint").innerHTML="";
-        return;
-    }
-    var xmlhttp=new XMLHttpRequest();
-    xmlhttp.onreadystatechange=function()
-    {
-        if (xmlhttp.readyState==4 && xmlhttp.status==200)
-        {
-            document.getElementById("txtHint").innerHTML=xmlhttp.responseText;
-        }
-    }
-    xmlhttp.open("GET","livesearch.php?q="+str,true);
-    xmlhttp.send();
-}
-</script>
+  	.bold-text {
+  	    font-weight: bold;
+  		text-decoration: none;
+  	}
+  </style>
 
+  
+<script>
+$(function() {
+	
+	$.widget( "app.autocomplete", $.ui.autocomplete, {
+		
+	        _renderItem: function( ul, item ) {
+
+	            // Replace the matched text with a custom span. This
+	            // span uses the class found in the "highlightClass" option.
+	            var re = new RegExp( "(" + this.term + ")", "gi" ),
+	                cls = this.options.highlightClass,
+	                template = "<span class='" + cls + "'>$1</span>",
+	                label = item.label.replace( re, template )
+	            return $("<li>") 
+     			   .html( label )
+                   .appendTo( ul );               
+	        }
+        
+	    });
+	
+	
+  function log( message ) {
+    $( "<div>" ).text( message ).prependTo( "#log" );
+    $( "#log" ).scrollTop( 0 );
+  }
+
+  $( "#search" ).autocomplete({
+	highlightClass: "bold-text",
+    source: "search_autocomplete.php",
+    minLength: 3,
+	delay: 200,
+    select: function( event, ui ) {
+      log( ui.item ?
+        "Selected: " + ui.item.value + " aka " + ui.item.id :
+        "Nothing selected, input was " + this.value );
+    }
+  });
+});
+  </script>	
+
+</head>
+<body>
 
 <?php
 
 require "db_login.inc";
 
-$connection = mysql_connect($host,$user,$password) or die ("couldn't connect to the server");
-$db = mysql_select_db($database,$connection) or die ("couldn't connect to  the database");
+$connection = mysqli_connect($host,$user,$password) or die ("couldn't connect to the server");
+$db = mysqli_select_db($connection,$database) or die ("couldn't connect to the database");
 
     $value = $_GET["search"];
     $count = $_GET["num"];
@@ -75,8 +104,8 @@ $db = mysql_select_db($database,$connection) or die ("couldn't connect to  the d
     $query = "SELECT title, abstract, wordfile, ((3 *(MATCH (title) AGAINST ('$value')))+(MATCH (abstract) AGAINST ('$value'))) AS score FROM recipeTable WHERE MATCH (title,abstract,wordfile,purpose,keywords) AGAINST ('$value') ORDER BY score DESC";
     
     
-    $result = mysql_query($query) or die("couldn't execute query");
-    while ($row = mysql_fetch_array($result,MYSQL_ASSOC))
+    $result = mysqli_query($connection,$query) or die("couldn't execute query");
+    while ($row = mysqli_fetch_array($result,MYSQLI_ASSOC))
     {
         EXTRACT($row);
         $counter = $counter +1;
@@ -97,23 +126,17 @@ echo "</ul>";
     
     if ($count > 0) {echo "<a href='searchprocess3.php?search=";echo"$value";echo"&num=";echo"$prevcounter";echo"'>Previous 20</a> | ";}
     
-    
 echo "<a href='searchprocess3.php?search=";echo"$value";echo"&num=";echo"$nextcounter";echo"'>Next 20</a><br><br>";
     
-echo "<form action='search.php' method='GET' autocomplete='off'>
-	<input list='txtHint' type='text' name='search' size='65' onkeyup='showHint(this.value)'>
-    <datalist id='txtHint'><option value=' '>
-    </datalist>
-</form>
-<br>";
+?>
 
+<form action="search.php" method="GET" autocomplete="off">
+<div class="ui-widget"><input id="search" type="text" name="search" size="65" autocomplete="off" autofocus>
+        <input type="hidden" name="num" value="0">
+        <input type="submit" value="submit"> </div> 
+</form><br>
 
-
+<?php
 echo "</body></html>";
 
 ?>
-
-
-
-
-
